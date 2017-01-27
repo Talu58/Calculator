aa8abf0915b0b698abeaa68fb1493f81b78370fd
Romain DEJEAN <romain.dejean@gadz.org>
2017-01-26 16:45:00 -0800

    refactored operation and numbers class

--- app.js
+++ app.js
@@ -1,72 +1,12 @@
 $(document).ready(function() {
   let str = '';
 
-  $('#0').on('click', function(e) {
+  $('.number').on('click', function(e) {
     str += (e.target.innerHTML);
     $('#display').html(str);
   });
 
-  $('#1').on('click', function(e) {
-    str += (e.target.innerHTML);
-    $('#display').html(str);
-  });
-
-  $('#2').on('click', function(e) {
-    str += (e.target.innerHTML);
-    $('#display').html(str);
-  });
-
-   $('#3').on('click', function(e) {
-    str += (e.target.innerHTML);
-    $('#display').html(str);
-  });
-
-   $('#4').on('click', function(e) {
-    str += (e.target.innerHTML);
-    $('#display').html(str);
-  });
-
-   $('#5').on('click', function(e) {
-    str += (e.target.innerHTML);
-    $('#display').html(str);
-  });
-
-   $('#6').on('click', function(e) {
-    str += (e.target.innerHTML);
-    $('#display').html(str);
-  });
-
-   $('#7').on('click', function(e) {
-    str += (e.target.innerHTML);
-    $('#display').html(str);
-  });
-
-   $('#8').on('click', function(e) {
-    str += (e.target.innerHTML);
-    $('#display').html(str);
-  });
-
-   $('#9').on('click', function(e) {
-    str += (e.target.innerHTML);
-    $('#display').html(str);
-  });
-
-   $('#add').on('click', function(e) {
-    str += (e.target.innerHTML);
-    $('#display').html(str);
-  });
-
-   $('#minus').on('click', function(e) {
-    str += (e.target.innerHTML);
-    $('#display').html(str);
-  });
-
-  $('#multiply').on('click', function(e) {
-    str += (e.target.innerHTML);
-    $('#display').html(str);
-  });
-  
-  $('#divide').on('click', function(e) {
+   $('.operation').on('click', function(e) {
     str += (e.target.innerHTML);
     $('#display').html(str);
   });
